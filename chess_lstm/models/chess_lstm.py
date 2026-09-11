74bb4e82eaca3ec9370cae68c450fb81654ec912
Igor <l>
2026-09-11 15:26:08 +0300

    hidden_size 128 -> 256

--- chess_lstm/models/chess_lstm.py
+++ chess_lstm/models/chess_lstm.py
@@ -19,7 +19,7 @@ class ChessLSTM(nn.Module):
         self,
         vocab_size: int,
         emb_dim: int = 64,
-        hidden_size: int = 128,
+        hidden_size: int = 256,
         num_layers: int = 2,
         dropout: float = 0.1
     ):
